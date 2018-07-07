0e32742b3a30c56a185c5ca8548228281132d2aa
erfan <erfan_1376_rahimizadeh@yahoo.com>
2018-07-07 23:02:20 +0430

    test

--- src/Engine/Main.java
+++ src/Engine/Main.java
@@ -10,27 +10,27 @@ import javax.swing.JFrame;
  */
 public class Main {
 	
-    public static void main(String[] args) {
-		// Initialize the global thread-pool
-		ThreadPool.init();
-		
-		// Show the game menu ...
-		
-		// After the player clicks 'PLAY' ...
-		EventQueue.invokeLater(new Runnable() {
-			@Override
-			public void run() {
-				GameFrame frame = new GameFrame("Tanki !");
-				frame.setLocationRelativeTo(null); // put frame at center of screen
-				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-				frame.setVisible(true);
-				frame.initBufferStrategy();
-				// Create and execute the game-loop
-				GameLoop game = new GameLoop(frame);
-				game.init();
-				ThreadPool.execute(game);
-				// and the game starts ...
-			}
-		});
-    }
+//    public static void main(String[] args) {
+//		// Initialize the global thread-pool
+//		ThreadPool.init();
+//
+//		// Show the game menu ...
+//
+//		// After the player clicks 'PLAY' ...
+//		EventQueue.invokeLater(new Runnable() {
+//			@Override
+//			public void run() {
+//				GameFrame frame = new GameFrame("Tanki !");
+//				frame.setLocationRelativeTo(null); // put frame at center of screen
+//				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//				frame.setVisible(true);
+//				frame.initBufferStrategy();
+//				// Create and execute the game-loop
+//				GameLoop game = new GameLoop(frame);
+//				game.init();
+//				ThreadPool.execute(game);
+//				// and the game starts ...
+//			}
+//		});
+//    }
 }
