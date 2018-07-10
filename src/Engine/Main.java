53eecf945a628c09512b30153aa2935472078650
Arya Khaligh <bartararya@gmail.com>
2018-07-10 11:34:03 +0330

    Forgotten sound folder is added to git

--- src/Engine/Main.java
+++ src/Engine/Main.java
@@ -16,24 +16,24 @@ public class Main {
 		// Initialize the global thread-pool
 		ThreadPool.init();
 
-		Start start=new Start();
+//		Start start=new Start();
 
 		// After the player clicks 'PLAY' ...
-//		EventQueue.invokeLater(new Runnable() {
-//			@Override
-//			public void run() {
-//				GameFrame frame = new GameFrame("Tanki !");
-//				frame.setLocationRelativeTo(null); // put frame at center of screen
-//				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//				frame.setVisible(true);
-//				frame.initBufferStrategy();
-//				// Create and execute the game-loop
-//				GameLoop game = new GameLoop(frame);
-//				game.init();
-//				ThreadPool.execute(game);
-//				// and the game starts ...
-//			}
-//		});
+		EventQueue.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				GameFrame frame = new GameFrame("Tanki !");
+				frame.setLocationRelativeTo(null); // put frame at center of screen
+				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				frame.setVisible(true);
+				frame.initBufferStrategy();
+				// Create and execute the game-loop
+				GameLoop game = new GameLoop(frame);
+				game.init();
+				ThreadPool.execute(game);
+				// and the game starts ...
+			}
+		});
     }
 
 
